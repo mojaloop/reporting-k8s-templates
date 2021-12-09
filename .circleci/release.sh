f90d0c4bdbf773bb147cc5b747a36c7d91550096
Yevhen Kyriukha <kirgene@gmail.com>
2021-12-09 05:40:31 +0200

    Fix helm chart release

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -5,6 +5,7 @@ set -o nounset
 set -o pipefail
 
 : "${GITHUB_TOKEN:?Environment variable GITHUB_TOKEN must be set}"
+: "${CR_REPO_URL:?Environment variable CR_REPO_URL must be set}"
 : "${GIT_USERNAME:?Environment variable GIT_USERNAME must be set}"
 : "${GIT_REPOSITORY_NAME:?Environment variable GIT_REPOSITORY_NAME must be set}"
 
@@ -19,24 +20,21 @@ main() {
 
     echo "Fetching charts..."
 
-    local changed_charts=()
+    local changed
+    local chart_name
+    local chart_ver
+    local tag
 
-    # iterate over all charts and skip those that already have a tag matching the current version
-    for chart_config in */Chart.yaml; do
-        local chart_name
-        local chart_ver
-        local tag
-
-        chart_name=$(awk '/^name: /{print $NF}' < "$chart_config" )
-        chart_ver=$(awk '/^version: /{print $NF}' < "$chart_config")
-        tag="${chart_name}-${chart_ver}"
-        if git rev-parse "$tag" >/dev/null 2>&1; then
-            echo "Chart '$chart_name': tag '$tag' already exists, skipping."
-        else
-            echo "Chart '$chart_name': new version '$chart_ver' detected."
-            changed_charts+=("$chart_name")
-        fi
-    done
+    chart_name=$(awk '/^name: /{print $NF}' < Chart.yaml )
+    chart_ver=$(awk '/^version: /{print $NF}' < Chart.yaml)
+    tag="${chart_name}-${chart_ver}"
+    if git rev-parse "$tag" >/dev/null 2>&1; then
+	    echo "Chart '$chart_name': tag '$tag' already exists, skipping."
+	    changed=false
+    else
+	    echo "Chart '$chart_name': new version '$chart_ver' detected."
+	    changed=true
+    fi
 
     # preparing dirs
     rm -rf .cr-release-packages
@@ -45,12 +43,9 @@ main() {
     rm -rf .cr-index
     mkdir -p .cr-index
 
-    # only release those charts for which a new version has been detected
-    if [[ -n "${changed_charts[*]}" ]]; then
-        for chart in "${changed_charts[@]}"; do
-            echo "Packaging chart '$chart'..."
-            package_chart "$chart"
-        done
+    if $changed; then
+	echo "Packaging chart '$chart_name'..."
+	helm package . --destination .cr-release-packages --dependency-update
 
         release_charts
 
@@ -65,17 +60,12 @@ main() {
     popd > /dev/null
 }
 
-package_chart() {
-    local chart="$1"
-    helm package "$chart" --destination .cr-release-packages --dependency-update
-}
-
 release_charts() {
     cr upload -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME"
 }
 
 update_index() {
-    cr index -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME"
+    cr index -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -c "$CR_REPO_URL"
 
     git config user.email "$GIT_USERNAME@users.noreply.github.com"
     git config user.name "$GIT_USERNAME"
